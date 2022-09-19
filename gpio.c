0f77d53024b2f4f0ff339e41bd56bf5301202798
Dakota-Augenstein <113377909+Dakota-Augenstein@users.noreply.github.com>
2022-09-19 11:35:02 -0600

    Update gpio.c

--- gpio.c
+++ gpio.c
@@ -23,3 +23,43 @@ void gpio_toggle_red_led(){
     for(i = 200000; i > 0; i--);
 }
 */
+
+void count_down(void){
+/*
+ * count_down circuit
+ *
+ * created September 19, 2022
+ * Author: Dakota Augenstein
+ *
+ * This Driver is intended to turn on 3 LEDS that are green yellow and red
+ * it will start by turning them all on and will then turn them off similarly to a drag strips lights
+ */
+
+    //int LED_finished = 0; //could be used as an indicator to see if the driver has finsihed
+
+    P6->DIR |= BIT7;
+    P2->DIR |= BIT3;
+    P5->DIR |= BIT1; //sets pins as outputs
+
+    P6->OUT |= BIT7;
+    P2->OUT |= BIT3;
+    P5->OUT |= BIT1; //turns all leds on
+
+    int i = 0;
+    for(i = 0; i < 30000; i++);
+    i = 0; //delay
+
+    P5->OUT ^= BIT1;
+    for(i = 0; i < 30000; i++);
+    i = 0; //turns off red
+
+    P2->OUT ^= BIT3;
+    for(i = 0; i < 30000; i++);
+    i = 0; //turns off yellow
+
+    P6->OUT ^= BIT7;
+    for(i = 0; i < 30000; i++);
+    i = 0; //turns off green
+
+    //LED_finished = 1;
+}
